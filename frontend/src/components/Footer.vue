01895f196bd123677c0954786cb8aa09630831a1
Warm Beer <mickvd99@gmail.com>
2022-03-08 13:28:58 +0100

    style: footer made text darker

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-sm leading-6 mt-12">
-    <div class="pt-10 pb-28 border-t sm:flex justify-end text-slate-400 border-slate-200/5">
+    <div class="pt-10 pb-28 border-t sm:flex justify-end text-slate-600 border-slate-200/5">
 <!--      <div class="mb-6 sm:mb-0 sm:flex">-->
 <!--        <a class="hover:text-slate-400" href="">Terms of Service</a>-->
 <!--        <p class="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200/5"><a class="hover:text-slate-400" href="">Privacy Policy</a></p>-->
