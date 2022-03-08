faa1e12431bc7538b2a5708111124f5b697afc31
Warm Beer <mickvd99@gmail.com>
2022-03-08 12:51:06 +0100

    style: footer added nautilus link

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="text-sm leading-6 mt-12">
-    <div class="pt-10 pb-28 border-t sm:flex justify-end text-slate-500 border-slate-200/5">
+    <div class="pt-10 pb-28 border-t sm:flex justify-end text-slate-400 border-slate-200/5">
 <!--      <div class="mb-6 sm:mb-0 sm:flex">-->
 <!--        <a class="hover:text-slate-400" href="">Terms of Service</a>-->
 <!--        <p class="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200/5"><a class="hover:text-slate-400" href="">Privacy Policy</a></p>-->
 <!--      </div>-->
-      <a class="hover:text-slate-400" href="https://github.com/torrust/torrust">Powered by Torrust</a>
+      <a class="hover:text-white transition duration-200" href="https://github.com/torrust/torrust">Powered by Torrust</a>
+      <a class="ml-2 pl-2 border-l border-slate-600 hover:text-white transition duration-200" href="https://nautilus-cyberneering.de">Nautilus Cyberneering GmbH</a>
     </div>
   </div>
 </template>
